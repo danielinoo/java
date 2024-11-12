8aa3875662afba5264e117037b5c46a1a8256bc3
danielinoo <daniele.pietropaolo1234@gmail.com>
2024-11-12 15:02:22 +0100

    persone

--- persone/Persona.java
+++ persone/Persona.java
@@ -1,35 +1,34 @@
-// package Persone;
 
 
-// public class Persona {
+public class Persona {
 	
-// 	private String nome;
-// 	private String cognome;
-// 	public Persona(String nome, String cognome) {
-// 		super();
-// 		this.nome = nome;
-// 		this.cognome = cognome;
-// 	}
-// 	public String getNome() {
-// 		return nome;
-// 	}
-// 	public void setNome(String nome) {
-// 		this.nome = nome;
-// 	}
-// 	public String getCognome() {
-// 		return cognome;
-// 	}
-// 	public void setCognome(String cognome) {
-// 		this.cognome = cognome;
-// 	}
+	private String nome;
+	private String cognome;
+	public Persona(String nome, String cognome) {
+		super();
+		this.nome = nome;
+		this.cognome = cognome;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
 	
-// 	@Override
-// 	public String toString() {
-// 		return "Persona [nome=" + nome + ", cognome=" + cognome + "]";
-// 	}
+	@Override
+	public String toString() {
+		return "Persona [nome=" + nome + ", cognome=" + cognome + "]";
+	}
 	
 	
 	
 
 	
-// }
+}
